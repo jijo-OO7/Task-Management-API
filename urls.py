ae29a2de9a6c8e13b95261299f6fd0881940bd93
Suman Mandal <106513760+jijo-OO7@users.noreply.github.com>
2024-11-16 16:52:30 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import (
+    TaskListCreateView,
+    TaskRetrieveUpdateDestroyView,
+    TaskCompleteView
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('tasks.urls')),
+]
+
+
+urlpatterns = [
+    path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
+    path('tasks/<int:pk>/', TaskRetrieveUpdateDestroyView.as_view(), name='task-detail'),
+    path('tasks/<int:pk>/complete/', TaskCompleteView.as_view(), name='task-complete'),
 ]
